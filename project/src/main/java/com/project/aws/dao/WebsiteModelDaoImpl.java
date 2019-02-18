ac6a46380d4646f7592f63df5586e6b7625a50d9
hiennguyen@bookygroup.fi <hienminhnguyen711@gmail.com>
2019-02-18 16:00:42 +0200

    Add to website model dao

--- project/src/main/java/com/project/aws/dao/WebsiteModelDaoImpl.java
+++ project/src/main/java/com/project/aws/dao/WebsiteModelDaoImpl.java
@@ -22,6 +22,18 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 public class WebsiteModelDaoImpl implements WebsiteModelDao{
 
+    // AWS credentials
+
+    // credentials for the client come from the environment variables pre-configured by Lambda. These are tied to the
+    // Lambda function execution role.new BasicAWSCredentials(amazonAWSAccessKey,amazonAWSSecretKey)
+    private static AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient();
+
+    // Con be nay thong minh vcl ra
+    
+
+
+
+
     @Override
     public WebsiteModel getByUrl(String url) {
         return null;
@@ -43,5 +55,19 @@ public class WebsiteModelDaoImpl implements WebsiteModelDao{
     }
 
 
-    // ngoi chui tui troi oi - 
+    // ngoi chui tui troi oi -
+
+
+
+    // sao con nho nay han thong minh ghe nhi - may khong qua duoc dan chau a dau
+
+
+
+    // No van binh thuong ma
+
+
+    // Gui lai cho no
+
+
+
 }

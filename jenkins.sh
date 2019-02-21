e912510d77e8bd3fbf1c89a2aad775e7e65f092e
hiennguyen@bookygroup.fi <hienminhnguyen711@gmail.com>
2019-02-21 10:47:52 +0200

    Add jenkins file

---
+++ jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Kill ongoing process 
+kill `cat run.pid` || true
+# Kill process 
+pkill java || true
+
+
+# User home 
+USER_HOME="$(echo -n $(bash -c "cd ~${USER} && pwd"))"
+
+
+FRONTEND_HOME=file:///home/loyalty/frontend
+
+SAMPLE_DB_DATA_PATH=classpath:flyway/sample
+
+
+nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9999 -jar -Dspring.profiles.active=prod -Dloyalty-program.db.samplesPath="$SAMPLE_DB_DATA_PATH" -Dspring.resources.static-locations="$FRONTEND_HOME" loyalty-program-api-0.0.1-SNAPSHOT.jar > $USER_HOME/logs/server.log & echo $! > run.pid
+
+
+
+
+
+
+
